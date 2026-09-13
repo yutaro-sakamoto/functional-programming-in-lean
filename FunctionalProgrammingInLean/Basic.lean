9699f0ebf54929c4cd59adbc99d9114c1308bfd5
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2026-09-13 17:39:54 +0900

    wip: wip

--- FunctionalProgrammingInLean/Basic.lean
+++ FunctionalProgrammingInLean/Basic.lean
@@ -1,1 +1,9 @@
 def hello := "world"
+
+class HPlus (α : Type) (β : Type) (γ : Type) where
+    hPlus : α → β → γ
+
+instance [Add α] : HPlus α α α where
+    hPlus := Add.add
+
+#check HPlus.hPlus (3 : Nat) (5 : Nat)
